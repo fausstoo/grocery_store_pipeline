59f3ce4b47f97d5811a617fe2785727898de4bf8
FaustoPF <faustopuchetafortin@gmail.com>
2023-10-30 20:40:54 -0300

    Finished final details on data_transformation.py

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -26,64 +26,76 @@ class DataTransformation:
     def __init__(self, config: DataTransformationConfig):
         self.config = config
         
-        
+    logging.info("Data cleaning started...")
     def clean_data(self):
+        logging.info("Removing unnecessary columns...")
         self.drop_columns()
+        
+        logging.info("Filling NaN values...")
         self.fill_na_values()
+        
+        logging.info("Removing remaining NaN values...")
         self.drop_na()
+        
+        logging.info("Transforming 'date' column...")
         self.transform_date_columns()
+        
+        logging.info("Transforming numeric columns...")
         self.transform_numeric_columns()
               
     def fill_na_values(self):
-        # Fill NaN values in 'platform' column
-        platform_col = 'platform'
-        if platform_col in self.config.data.columns:
-            self.config.data[platform_col] = self.config.data[platform_col].fillna("")
+        try:    
+            # Fill NaN values in 'platform' column
+            platform_col = 'platform'
+            if platform_col in self.config.data.columns:
+                self.config.data[platform_col] = self.config.data[platform_col].fillna("")
 
-        # Fill NaN values in 'detail' column
-        detail_col = 'detail'
-        if detail_col in self.config.data.columns:
-            self.config.data[detail_col] = self.config.data[detail_col].fillna("")
+            # Fill NaN values in 'detail' column
+            detail_col = 'detail'
+            if detail_col in self.config.data.columns:
+                self.config.data[detail_col] = self.config.data[detail_col].fillna("")
+        except CustomException as e:
+            raise CustomException(sys, e)
                     
     def drop_na(self):
-        self.config.data.dropna(subset=self.config.data.columns, inplace=True)
-                            
+        try:    
+            self.config.data.dropna(subset=self.config.data.columns, inplace=True)
+        except CustomException as e:
+            raise CustomException(sys, e)
+                        
     def drop_columns(self):
-        for col in self.config.columns_to_drop:
-            if col in self.config.data.columns:
-                self.config.data.drop(col, axis=1, inplace=True)
-
+        try:
+            for col in self.config.columns_to_drop:
+                if col in self.config.data.columns:
+                    self.config.data.drop(col, axis=1, inplace=True)
+        except CustomException as e:
+            raise CustomException(sys, e)
+        
     def transform_date_columns(self):
-        for col in self.config.date_columns:
-            if col in self.config.data.columns:
-                self.config.data[col] = self.config.data[col].str.split(' ').str[0]
-                self.config.data[col] = pd.to_datetime(self.config.data[col], format='%Y-%m-%d')
-
+        try:
+            for col in self.config.date_columns:
+                if col in self.config.data.columns:
+                    self.config.data[col] = self.config.data[col].str.split(' ').str[0]
+                    self.config.data[col] = pd.to_datetime(self.config.data[col], format='%Y-%m-%d')
+        except CustomException as e:
+            raise CustomException(sys, e)
+        
     def transform_numeric_columns(self):
-        for col in self.config.int_columns:
-            if col in self.config.data.columns:
-                self.config.data[col] = self.config.data[col].astype(int)
-        for col in self.config.float_columns:
-            if col in self.config.data.columns:
-                self.config.data[col] = self.config.data[col].astype(float)
-                
-    def fill_na_values(self):
-        # Fill NaN values in 'platform' column
-        platform_col = 'platform'
-        if platform_col in self.config.data.columns:
-            self.config.data[platform_col] = self.config.data[platform_col].fillna("")
-
-        # Fill NaN values in 'detail' column
-        detail_col = 'detail'
-        if detail_col in self.config.data.columns:
-            self.config.data[detail_col] = self.config.data[detail_col].fillna("")
-            
+        try:
+            for col in self.config.int_columns:
+                if col in self.config.data.columns:
+                    self.config.data[col] = self.config.data[col].astype(int)
+            for col in self.config.float_columns:
+                if col in self.config.data.columns:
+                    self.config.data[col] = self.config.data[col].astype(float)
+        except CustomException as e:
+            raise CustomException(sys, e)            
             
             
 if __name__ == "__main__":
+    
     # SALES table
     sales_df = pd.read_csv("./data/CSV_tables/sales.csv")
-    
     sales_config = DataTransformationConfig(
         data=sales_df,
         columns_to_drop=['product_code', 'category', 'product', 'unit_price', 'Precio de Costo', 'Total COGS', 'Ganancia Neta'],
@@ -99,7 +111,6 @@ if __name__ == "__main__":
     print(sales_config.data.head())
     
     # PRODUCTS RECIEVED table
-    
     pr_df = pd.read_csv("./data/CSV_tables/products_recieved.csv")
     pr_config = DataTransformationConfig(
         data=pr_df,
@@ -117,7 +128,6 @@ if __name__ == "__main__":
     
     # TRANSACTIONS
     tr_df = pd.read_csv("./data/CSV_tables/transactions.csv")
-    
     tr_config = DataTransformationConfig(
         data=tr_df,
         columns_to_drop=[],
